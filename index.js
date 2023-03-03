db488c664f875786cb52245d8ad7ca392b68fceb
ciaran <ciaranjotter@gmail.com>
2023-03-03 10:25:34 +0200

    Initial setup of express and socket

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function openDiscord() {
     console.log("test");
     window.open("https://discord.gg/hZK5ThmnG3", "_blank");
 }
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer();
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello world!</h1>");
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+})
