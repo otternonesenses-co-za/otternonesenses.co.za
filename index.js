5fe01805b268296c0a26126922fbbaaf6c2fbe5b
ciaran <ciaranjotter@gmail.com>
2023-03-03 10:30:04 +0200

    update to port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function openDiscord() {
-    console.log("test");
-    window.open("https://discord.gg/hZK5ThmnG3", "_blank");
-}
+// function openDiscord() {
+//     console.log("test");
+//     window.open("https://discord.gg/hZK5ThmnG3", "_blank");
+// }
 
 const express = require('express');
 const app = express();
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
     res.send("<h1>Hello world!</h1>");
 });
 
-server.listen(3000, () => {
+server.listen(80, () => {
     console.log('listening on *:3000');
 })
