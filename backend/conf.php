a2752f2a721b62c05878dfae6e073fb9c473593f
ciaran <ciaranjotter@gmail.com>
2023-03-04 09:27:12 +0200

    starting the backend

--- backend/conf.php
+++ backend/conf.php
@@ -1,12 +1,20 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 header("Access-Control-Allow-Origin: *");
 
     $user = 'root';
     $pass = 'Tigerman6';
-    $host = 'localhost';
-    $dn_name = 'otter_chat';
+    $host = 'otternonesenses.co.za';
+    $db_name = 'otter_chat';
 
-    $db = new mysqli($host, $user,$pass, $db_name) or die ("die connection failed");
+    $db = mysqli_connect($host, $user,$pass, $db_name)
+
+    if ($db = mysqli_connect($host, $user,$pass, $db_name)) {
+        echo "connected successfully";
+    } else {
+        die("connection fauiled");
+    }
 
 ?>
