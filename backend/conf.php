1b9dfba53e82bb065104f86293e15eec02a30a0d
ciaran <ciaranjotter@gmail.com>
2023-03-04 10:11:28 +0200

    i dont really know anymore

--- backend/conf.php
+++ backend/conf.php
@@ -9,9 +9,9 @@ header("Access-Control-Allow-Origin: *");
     $host = 'localhost';
     $db_name = 'otter_chat';
 
-    $db = new mysqli($host, $user, $pass, $db_name);
-    if (!$db) {
-        die ("connection failed");
+    $db = mysqli_connect($host, $user, $pass, $db_name);
+    if ($db->connect_error) {
+        die ("connection failed: " . $db->connect_error);
     }
 
 ?>
