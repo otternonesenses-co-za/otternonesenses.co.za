d48f2b51ada5cb903902e8faddc2aa8eff63bc1b
ciaran <ciaranjotter@gmail.com>
2023-03-04 09:37:37 +0200

    adding the ackend register

--- backend/register.php
+++ backend/register.php
@@ -2,14 +2,14 @@
 
     include('conf.php');
 
-    // if (isset($_GET['name']) && isset($_GET['surname']) && isset($_GET['user']) && isset($_GET['pass'])) {
-    //     $name  = $_GET['name'];
-    //     $surname = $_GET['surname'];
-    //     $username = $_GET['user'];
-    //     $password = $_GET['pass'];
+    if (isset($_GET['name']) && isset($_GET['surname']) && isset($_GET['user']) && isset($_GET['pass'])) {
+        $name  = $_GET['name'];
+        $surname = $_GET['surname'];
+        $username = $_GET['user'];
+        $password = $_GET['pass'];
 
-    //     $query = "INSERT INTO accounts (name, surname, username, password) VALUES ('$name','$surname','$username','$password)";
-    //     $res = $db->query($query);
-    // }
+        $query = "INSERT INTO accounts (name, surname, username, password) VALUES ('$name','$surname','$username','$password)";
+        $res = $db->query($query);
+    }
 
 ?>
