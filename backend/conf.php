f47822b8ea393eaf2cc750e4325614e1b2ac1207
ciaran <ciaranjotter@gmail.com>
2023-03-04 09:34:46 +0200

    bug fix

--- backend/conf.php
+++ backend/conf.php
@@ -6,10 +6,8 @@ header("Access-Control-Allow-Origin: *");
 
     $user = 'root';
     $pass = 'Tigerman6';
-    $host = 'otternonesenses.co.za';
+    $host = 'localhost';
     $db_name = 'otter_chat';
-
-    $db = mysqli_connect($host, $user,$pass, $db_name)
 
     if ($db = mysqli_connect($host, $user,$pass, $db_name)) {
         echo "connected successfully";
