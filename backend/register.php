d0d368c5c167495ded1d454685c08492a0fedf6d
ciaran <ciaranjotter@gmail.com>
2023-03-04 09:38:51 +0200

    bug fix

--- backend/register.php
+++ backend/register.php
@@ -8,7 +8,7 @@
         $username = $_GET['user'];
         $password = $_GET['pass'];
 
-        $query = "INSERT INTO accounts (name, surname, username, password) VALUES ('$name','$surname','$username','$password)";
+        $query = "INSERT INTO accounts (name, surname, username, password) VALUES ('$name','$surname','$username','$password')";
         $res = $db->query($query);
     }
 
