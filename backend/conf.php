744f3a55850dd1c5c4a5c3201be390272e017364
ciaran <ciaranjotter@gmail.com>
2023-03-04 10:14:21 +0200

    small fix

--- backend/conf.php
+++ backend/conf.php
@@ -9,7 +9,7 @@ header("Access-Control-Allow-Origin: *");
     $host = 'localhost';
     $db_name = 'otter_chat';
 
-    $db = mysqli_connect($host, $user, $pass, $db_name);
+    $db = mysql_connect($host, $user, $pass, $db_name);
     if ($db->connect_error) {
         die ("connection failed: " . $db->connect_error);
     }
